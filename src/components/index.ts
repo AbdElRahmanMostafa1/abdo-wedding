c654838e4baa42bdda056f5494064347705263d6
AbdElRahmanMostafa1 <abdelrahman22.mostafa@gmail.com>
2024-04-18 18:53:19 +0200

    delete image slider

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 import Navbar from "./ui/navbar";
 import CountdownItem from "./ui/countdownItem";
-import ImageSlider from "./ui/image-slider";
 import Footer from "./ui/footer";
 
 import WeddingLocation from "./wedding-location";
@@ -8,7 +7,6 @@ import WeddingLocation from "./wedding-location";
 export {
     Navbar,
     CountdownItem,
-    ImageSlider,
     WeddingLocation,
     Footer
 }
--- src/sections/ourPhotos/index.tsx
+++ src/sections/ourPhotos/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { ImageSlider } from "@/components";
 import image1 from "../../../public/images/1.jpg";
 import image2 from "../../../public/images/2.jpg";
 import image3 from "../../../public/images/3.jpg";
@@ -9,7 +8,6 @@ const OurPhotos = () => {
   return (
     <div>
       OurPhotos
-      <ImageSlider images={images} />
     </div>
   );
 };
